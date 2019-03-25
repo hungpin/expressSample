de02fd6654330278746082266a4e2bb5c9d5b0c3
阿超 <47099871+AZTLEricTsai@users.noreply.github.com>
2019-03-25 16:25:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res)=>{
-  res.json({"tilte":"Sample Express"});
+  res.json({"tilte":"Fuck You"});
 });
 
 app.listen(8080);
